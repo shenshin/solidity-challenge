ece645977090c3aa712b58bfa2437412463b9fde
Aleksandr Shenshin <shenshin@me.com>
2023-03-01 23:55:16 +0100

    delete extra dependency

--- contracts/PurrNFT.sol
+++ contracts/PurrNFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.9;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 
