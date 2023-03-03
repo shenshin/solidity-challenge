cd0681e2ed7978f6f1372e16b6feeb986c718e23
Aleksandr Shenshin <shenshin@me.com>
2023-03-03 11:39:30 +0100

    remove package-lock from ignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import { mnemonic } from './.secret.json';
+// import { mnemonic } from './.secret.json';
 
-const accounts = {
+/* const accounts = {
   mnemonic,
   path: "m/44'/60'/0'/0",
-};
+}; */
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
     hardhat: {
-      accounts,
+      // accounts,
     },
     rskmainnet: {
       chainId: 30,
       url: 'https://public-node.rsk.co/',
-      accounts,
+      // accounts,
     },
     rsktestnet: {
       chainId: 31,
       url: 'https://public-node.testnet.rsk.co/',
-      accounts,
+      // accounts,
     },
   },
 };
