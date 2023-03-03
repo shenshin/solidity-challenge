eceb4280647f9a83db33a17cd20c0860b031ef8a
Aleksandr Shenshin <shenshin@me.com>
2023-03-03 10:57:30 +0100

    remove outdated comments

--- contracts/PurrNFT.sol
+++ contracts/PurrNFT.sol
@@ -22,7 +22,6 @@ contract PurrNFT is ERC721, Ownable {
   }
   // stores all whitelisted tokens
   Accepted[] public whiteList;
-  // get length function?
   mapping(IERC20 => bool) public isAccepted;
 
   uint256 public price = 1;
@@ -94,7 +93,6 @@ contract PurrNFT is ERC721, Ownable {
     // transfer `price` from every whitelisted token
     for (uint256 i = 0; i < whiteList.length; i++) {
       Accepted storage accepted = whiteList[i];
-      // request transfer approval from the smart wallet
       require(
         accepted.token.transferFrom(msg.sender, address(this), price),
         'PurrNFT: Cannot transfer tokens'
